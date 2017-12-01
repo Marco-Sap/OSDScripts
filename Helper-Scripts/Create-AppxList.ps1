75bf9f7639e69b0cec5476dfd0cc6e760ead4991
Marco Sap <Marco-Sap@users.noreply.github.com>
2017-12-01 14:24:08 +0100

    Add files via upload

---
+++ Helper-Scripts/Create-AppxList.ps1
@@ -0,0 +1,64 @@
+﻿<#
+.SYNOPSIS
+   Create the Appx list to be removed during Task Sequence
+.DESCRIPTION
+   Create a list with provisioned Appx applications to be remove during
+   a Task Sequence. The file Remove-Inbox-Apps.txt will be created on the
+   desktop. Edit the text file if needed and add it to your Scripts Folder.
+   Or create a Configration Manager package and use it with a Task sequence.
+.AUTHOR
+   Marco Sap 
+.VERSION
+   2.0.0
+.EXAMPLE
+   Create-AppxList
+.EXAMPLE
+   Create-AppxList -Overwrite -Verbose
+.DISCLAIMER
+   This script code is provided as is with no guarantee or waranty
+   concerning the usability or impact on systems and may be used,
+   distributed, and modified in any way provided the parties agree
+   and acknowledge that Microsoft or Microsoft Partners have neither
+   accountabilty or responsibility for results produced by use of
+   this script.
+
+   Microsoft will not provide any support through any means.
+#>
+[CmdletBinding()]
+  Param
+  (
+  [Switch]$Overwrite
+  )
+
+Function Create-AppxList {
+  [CmdletBinding()]
+  Param
+  ()
+  
+    # Look for a config file.
+    $configFile = "$env:USERPROFILE\Desktop\Remove-Inbox-Apps.txt"
+    if ((Test-Path -Path $configFile) -and (!($Script:PSBoundParameters["Overwrite"])))
+    {      
+      # Read the list
+      Write-Verbose "File already exists at $configFile"
+      $list = Get-Content $configFile
+      Write-Verbose "Apps in list: $($list.Count)"   
+    }
+    else
+    {
+      # Build the List
+      Write-Verbose "Building list of provisioned apps"
+      $list = @()
+      Get-AppxProvisionedPackage -Online | % { $list += $_.DisplayName }
+     
+      # Write the List
+      $FilePath = "$env:USERPROFILE\Desktop"
+      $configFile = "$FilePath\Remove-Inbox-Apps.txt"
+      $list | Set-Content $configFile
+      Write-Verbose "Wrote list of apps to $filePath\Remove-Inbox-Apps.txt"
+      Write-Verbose "Apps written to list: $($list.Count)"
+
+    }  
+}
+
+Create-AppxList
---
+++ Helper-Scripts/Invoke-ExtractCab.ps1
@@ -0,0 +1,54 @@
+﻿<#
+.SYNOPSIS
+   Expand Cab files in Folder to be used for LGPO
+.DESCRIPTION
+   Expand Cab files in seperate folders to be used in LGPO Solution.
+   Cab files are the default backup from AGMP or GPMC, but LGPO uses
+   the extracted folder structure to import the policy.
+.AUTHOR
+   Marco Sap 
+.VERSION
+   2.0.0
+.EXAMPLE
+   Invoke-ExtractCab
+.EXAMPLE
+   Invoke-ExtractCab -Verbose
+.DISCLAIMER
+   This script code is provided as is with no guarantee or waranty
+   concerning the usability or impact on systems and may be used,
+   distributed, and modified in any way provided the parties agree
+   and acknowledge that Microsoft or Microsoft Partners have neither
+   accountabilty or responsibility for results produced by use of
+   this script.
+
+   Microsoft will not provide any support through any means.
+#>
+[CmdletBinding()]
+  Param()
+
+Function Invoke-ExtractCab {
+[CmdletBinding()]
+Param()
+
+  # Get Cab Files from folder
+  $CabFiles = Get-ChildItem -Path "$PSScriptRoot" -Filter *.cab
+
+    ForEach ($CabFile in $CabFiles)
+    {
+
+    # Create folders per Cab file
+    Write-Verbose "Processing $PSScriptRoot\$CabFile"
+    $FolderName = (Get-Item "$PSScriptRoot\$CabFile").BaseName
+    if (!(Test-Path -path $PSScriptRoot\$FolderName)) {New-Item "$PSScriptRoot\$FolderName" -ItemType directory > $null}
+
+    # Use expand on Cab file
+    $Arguments = @("$CabFile", "$FolderName", "-F:*")
+    $Process = Start-Process -FilePath "$env:Systemroot\system32\expand.exe" -ArgumentList $Arguments -WindowStyle Hidden -Wait -PassThru 
+    
+    # Handle Exit from process
+    if ($Process.ExitCode -eq 0) {Write-Verbose "$CabFile has been successfully processed"} else {Write-Verbose "Exit Code $($Process.ExitCode) for $CabFile"}
+
+    }
+}
+
+Invoke-ExtractCab
