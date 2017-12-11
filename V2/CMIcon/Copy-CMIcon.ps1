ea729231f9043c0148b31aacd56114575a33dc8f
Marco Sap <Marco-Sap@users.noreply.github.com>
2017-12-11 08:34:58 +0100

    Add files via upload

--- V2/CMIcon/Copy-CMIcon.ps1
+++ V2/CMIcon/Copy-CMIcon.ps1
@@ -1,4 +1,27 @@
-﻿[CmdletBinding()]
+﻿<#
+.SYNOPSIS
+   Copy Configuration Manager ShortCut to Application Menu
+.DESCRIPTION
+   Copy the Configuration Manager Control Panel ShortCut to
+   the application menu for easy access.
+.AUTHOR
+   Marco Sap 
+.VERSION
+   2.0.0
+.EXAMPLE
+   Copy-CMIcon
+.DISCLAIMER
+   This script code is provided as is with no guarantee or waranty
+   concerning the usability or impact on systems and may be used,
+   distributed, and modified in any way provided the parties agree
+   and acknowledge that Microsoft or Microsoft Partners have neither
+   accountabilty or responsibility for results produced by use of
+   this script.
+
+   Microsoft will not provide any support through any means.
+#>
+
+[CmdletBinding()]
  Param()
 
 # ---------------------------------------------------------------------------
